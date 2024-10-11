add1541c1a11a67175b16cd2aa56d7d9163065c1
iaalvearp <iaalvearp@gmail.com>
2024-10-11 11:26:34 -0500

    Adding and setting midudev tailwind animation plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import animated from '@midudev/tailwind-animations'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -15,6 +17,5 @@ export default {
 			'pink': '#F4DEBE'
 		}
 	},
-	plugins: [
-	],
+	plugins: [animated],
 }
